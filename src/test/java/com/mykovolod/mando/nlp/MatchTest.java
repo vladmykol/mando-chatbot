530f39881d577b2c39acd24315e358dd8cb9413b
Vlad-M <volodymyr.mykolaichuk@wrisk.co>
2022-04-16 11:31:24 +0300

    other language option 'other'

--- src/test/java/com/mykovolod/mando/nlp/MatchTest.java
+++ src/test/java/com/mykovolod/mando/nlp/MatchTest.java
@@ -4,13 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
 
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@TestPropertySource(
-//        locations = "classpath:application-test.properties")
-//@AutoConfigureMockMvc
-//@RestClientTest
 class MatchTest {
 
     @Test
